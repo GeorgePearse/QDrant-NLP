b23ff45a05ca263857e02372a7ed20f17baa6a23
George Pearse <georgep@eng-d-ml-george-02.behold.ai>
2022-08-24 15:43:03 +0000

    so nearly a polished solution

--- pages/encode.py
+++ pages/encode.py
@@ -11,13 +11,16 @@ run_encoding = st.button('Encode')
 # dataset = load_dataset('ag_news')
 # dataset_df = pd.DataFrame(dataset['train'])
 
+sample_size = st.text_input('Sample Size')
+sample_size = 6000
+
 if run_encoding: 
     ag_news_df = pd.read_csv('./ag_news.csv')
     sentences = list(ag_news_df['text'])
     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-    sample_size = 6000
     sentences_sampled = sentences[:sample_size]
 
-    embeddings = model.encode(sentences_sampled)
-    np.save(f'ag_news_embeddings.npy', embeddings)
+    with st.spinner("One second.."):
+        embeddings = model.encode(sentences_sampled)
+        np.save(f'ag_news_embeddings.npy', embeddings)
