061a23c7289ba7e8cd8c1a3e65ac08cdd6f00aec
George Pearse <georgep@eng-d-ml-george-02.behold.ai>
2022-08-24 14:05:20 +0000

    ignores the qdrant collection

--- pages/encode.py
+++ pages/encode.py
@@ -6,12 +6,15 @@ import pandas as pd
 
 st.write('# Encoding Dataset')
 
-ag_news_df = pd.read_csv('./ag_news.csv')
-sentences = list(ag_news_df['text'])
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+run_encoding = st.button('Encode')
 
-sample_size = 6000
-sentences_sampled = sentences[:sample_size]
+if run_encoding: 
+    ag_news_df = pd.read_csv('./ag_news.csv')
+    sentences = list(ag_news_df['text'])
+    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-embeddings = model.encode(sentences_sampled)
-np.save(f'ag_news_embeddings.npy', embeddings)
+    sample_size = 6000
+    sentences_sampled = sentences[:sample_size]
+
+    embeddings = model.encode(sentences_sampled)
+    np.save(f'ag_news_embeddings.npy', embeddings)
--- pages/load_to_qdrant.py
+++ pages/load_to_qdrant.py
@@ -1,18 +1,35 @@
 import streamlit as st
 from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance
+
 import numpy as np
 
-st.write('Load to QDrant Collection')
+st.write('# Load to QDrant Collection')
 
 qdrant_client = QdrantClient(host="localhost", port=6333)
 
-batch_size = 100
+batch_size = 32
 
 vectors = np.load('ag_news_embeddings.npy')
 
-qdrant_client.upload_collection(
-    collection_name="ag-news-mini-lm",
-    vectors=vectors,  # batch_of_vectors,
-    batch_size=batch_size,
-    parallel=4,
-)
+collection_name = 'ag-news-collection'
+
+insert_into_collection = st.button('Insert into Collection')
+
+if insert_into_collection:
+
+    st.write(vectors.shape)
+    st.write(len(vectors[0]))
+
+    qdrant_client.recreate_collection(
+        collection_name=collection_name,
+        vector_size=len(vectors[0]),
+        distance=Distance.COSINE
+    )
+
+    qdrant_client.upload_collection(
+        collection_name=collection_name,
+        vectors=vectors,  # batch_of_vectors,
+        batch_size=batch_size,
+        parallel=4,
+    )
