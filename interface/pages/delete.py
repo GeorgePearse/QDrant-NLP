bfb4d872ea127a9dfe8dbd4a0d6764c3a22f03eb
George Pearse <georgep@eng-d-ml-george-02.behold.ai>
2022-08-25 10:52:43 +0000

    fixing paths for query deletion

--- interface/pages/delete.py
+++ interface/pages/delete.py
@@ -2,19 +2,22 @@ import os
 
 import streamlit as st
 
+from config import (
+    saved_queries_path,
+    query_results_path,
+ )
+
 st.write("# Delete Queries")
 
-saved_queries = "./pages/saved_queries/"
-query_results = "./pages/results/"
-examples = os.listdir(saved_queries)
+examples = os.listdir(saved_queries_path)
 file_to_delete = st.selectbox("Examples", examples)
 
 delete = st.button("Delete")
 
 if delete:
     try:
-        os.remove(f"{saved_queries}/{file_to_delete}")
-        os.remove(f"{query_results}/{file_to_delete.replace('json','csv')}")
+        os.remove(f"{saved_queries_path}/{file_to_delete}")
+        os.remove(f"{query_results_path}/{file_to_delete.replace('json','csv')}")
         st.write("Query and results deleted successfully.")
     except Exception as e:
         st.error("Files were not successfully deleted.")
