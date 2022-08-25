85a3aa308b1a97a930a5b51049ee7bc26d88a322
George Pearse <georgep@eng-d-ml-george-02.behold.ai>
2022-08-25 08:11:38 +0000

    almost got the fastapi container sorted

--- interface/config.py
+++ interface/config.py
@@ -3,5 +3,5 @@ import os
 # config.py is the approach that Superset takes, can't be that bad
 vector_db_host = os.environ.get("VECTOR_DB_HOST", "http://192.168.54.124:6333")
 fast_api_host = os.environ.get("FAST_API_HOST", "http://127.0.0.1:8004")
-query_results_path = "./pages/results/"
-saved_queries_path = "./pages/saved_queries/"
+query_results_path = "../results/"
+saved_queries_path = "../saved_queries/"
