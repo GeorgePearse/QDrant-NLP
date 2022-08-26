ab591cc0cb146d47ef4f31e228f9973aaee1dbe7
George Pearse <47161914+GeorgePearse@users.noreply.github.com>
2022-08-26 15:12:34 +0100

    Update config.py

--- interface/config.py
+++ interface/config.py
@@ -1,6 +1,11 @@
 import os
+import logging
 
 # config.py is the approach that Superset takes, can't be that bad
-vector_db_host = os.environ.get("VECTOR_DB_HOST")
+try:
+  vector_db_host = os.environ["VECTOR_DB_HOST"]
+except Exception as e:
+  logging.error(f'Please specify IP of host machine. Exception is {e}")
+                
 query_results_path = "./results/"
 saved_queries_path = "./queries/"
