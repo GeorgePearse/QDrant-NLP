ebc7fdd3bcd0696b7a5f903a81c6f3ce8c5a98d0
George Pearse <47161914+GeorgePearse@users.noreply.github.com>
2022-08-26 15:11:31 +0100

    Update config.py

--- interface/config.py
+++ interface/config.py
@@ -1,7 +1,6 @@
 import os
 
 # config.py is the approach that Superset takes, can't be that bad
-vector_db_host = os.environ.get("VECTOR_DB_HOST", "http://192.168.54.124:6333")
-fast_api_host = os.environ.get("FAST_API_HOST", "http://192.168.54.124:8000")
+vector_db_host = os.environ.get("VECTOR_DB_HOST")
 query_results_path = "./results/"
 saved_queries_path = "./queries/"
